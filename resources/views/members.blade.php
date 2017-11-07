0199e419e044610525ab0ab27d0e56e3ad0df40d
Nick Holbrook <ndh175@gmail.com>
2017-11-06 23:53:10 -0500

    Added Eboard Partial

--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -1,39 +1,7 @@
 @extends ('layouts.master')
 
 @section ('content')
-<div class="team-clean">
-    <div class="container">
-        <div class="intro">
-            <div class="row">
-                <div class="col-md-1"></div>
-                <div class="col-md-10">
-                    <h2 class="text-center" style="padding-top:20px;padding-bottom:20px;margin-bottom:20px;">E-Board </h2></div>
-                <div class="col-md-1" style="padding-top:40px;"><a href="#positions" data-toggle="collapse" aria-expanded="false" aria-controls="positions"><i class="glyphicon glyphicon-info-sign" data-toggle="collapse"></i></a></div>
-            </div>
-        </div>
-        @include ('layouts.eboard_info')
-        <div class="row people" style="background-color:rgb(38,38,38);padding:25px;border-radius:10px;color:rgb(255,255,255)">
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/2.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> Jacquelyn Tindall </h3>
-                <p class="title"> Vice President </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
-            </div>
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/luis.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> Luis Cervantes </h3>
-                <p class="title"> President </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
-            </div>
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/micheal.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> Michael Cloutier </h3>
-                <p class="title"> Treasurer </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
-            </div>
-        </div>
-    </div>
-</div>
+@include ('layouts.eboard')
 <div class="team-clean">
     <div class="container">
         <div class="intro">
