eea38d33d89ce9395f00ee95672259a14ee75fa7
Nick Holbrook <ndh175@gmail.com>
2017-11-07 00:00:35 -0500

    Border Radius Issue

--- resources/views/layouts/eboard.blade.php
+++ resources/views/layouts/eboard.blade.php
@@ -9,78 +9,80 @@
             </div>
         </div>
         @include ('layouts.eboard_info')
-        <div class="row people" style="background-color:rgb(38,38,38);padding:25px;border-radius:10px;color:rgb(255,255,255)">
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/2.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> Jacquelyn Tindall </h3>
-                <p class="title"> Vice President </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+        <div style="background-color:rgb(38,38,38);padding:25px;border-radius:10px;color:rgb(255,255,255)">
+            <div class="row people" style="background-color:rgb(38,38,38);padding:25px;color:rgb(255,255,255)">
+                <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/2.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
+                    <h3 class="name"> Jacquelyn Tindall </h3>
+                    <p class="title"> Vice President </p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
+                <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/luis.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
+                    <h3 class="name"> Luis Cervantes </h3>
+                    <p class="title"> President </p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
+                <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/micheal.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
+                    <h3 class="name"> Michael Cloutier </h3>
+                    <p class="title"> Treasurer </p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
             </div>
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/luis.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> Luis Cervantes </h3>
-                <p class="title"> President </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+            <div class="row people" style="background-color:rgb(38,38,38);padding:25px;color:rgb(255,255,255)">
+                <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/2.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
+                    <h3 class="name"> Jannah Metcalfe </h3>
+                    <p class="title"> Secretary </p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
+                <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/luis.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
+                    <h3 class="name"> Taylor Fournier </h3>
+                    <p class="title"> Secretary </p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
+                <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/micheal.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
+                    <h3 class="name"> Erin Herner </h3>
+                    <p class="title"> PR / Social Outreach </p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
             </div>
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/micheal.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> Michael Cloutier </h3>
-                <p class="title"> Treasurer </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+            <div class="row people" style="background-color:rgb(38,38,38);padding:25px;color:rgb(255,255,255)">
+                <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/2.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
+                    <h3 class="name"> Olivia Duffton </h3>
+                    <p class="title"> Fundraising </p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
+                <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/luis.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
+                    <h3 class="name"> N/A </h3>
+                    <p class="title"> Creative Director </p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
+                <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/micheal.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
+                    <h3 class="name"> Briana Sanchez </h3>
+                    <p class="title"> Safety Director </p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
             </div>
-        </div>
-        <div class="row people" style="background-color:rgb(38,38,38);padding:25px;border-radius:10px;color:rgb(255,255,255)">
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/2.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> Jannah Metcalfe </h3>
-                <p class="title"> Secretary </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
-            </div>
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/luis.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> Taylor Fournier </h3>
-                <p class="title"> Secretary </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
-            </div>
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/micheal.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> Erin Herner </h3>
-                <p class="title"> PR / Social Outreach </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
-            </div>
-        </div>
-        <div class="row people" style="background-color:rgb(38,38,38);padding:25px;border-radius:10px;color:rgb(255,255,255)">
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/2.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> Olivia Duffton </h3>
-                <p class="title"> Fundraising </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
-            </div>
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/luis.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> N/A </h3>
-                <p class="title"> Creative Director </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
-            </div>
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/micheal.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> Briana Sanchez </h3>
-                <p class="title"> Safety Director </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
-            </div>
-        </div>
-        <div class="row people" style="background-color:rgb(38,38,38);padding:25px;border-radius:10px;color:rgb(255,255,255)">
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/2.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> Tom Weddell </h3>
-                <p class="title"> Gear Manager </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
-            </div>
-            <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/micheal.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
-                <h3 class="name"> Grace Goodrich </h3>
-                <p class="title"> Gear Manager </p>
-                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+            <div class="row people" style="background-color:rgb(38,38,38);padding:25px;color:rgb(255,255,255)">
+                <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/2.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
+                    <h3 class="name"> Tom Weddell </h3>
+                    <p class="title"> Gear Manager </p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
+                <div class="col-md-4 col-sm-6 item"><img class="img-circle" src="assets/img/micheal.jpg" style="width:150px;height:150px;object-fit:cover;overflow:hidden;">
+                    <h3 class="name"> Grace Goodrich </h3>
+                    <p class="title"> Gear Manager </p>
+                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                </div>
             </div>
         </div>
     </div>
