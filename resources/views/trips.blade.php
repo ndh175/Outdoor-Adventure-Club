e24ff7d6bd16846dbf96e7dd811768237109d5b1
Nick Holbrook <ndh175@gmail.com>
2017-11-07 23:39:49 -0500

    Update

--- resources/views/trips.blade.php
+++ resources/views/trips.blade.php
@@ -5,7 +5,7 @@
     @include ('trips.trip')
 @endforeach
 @foreach ($past_trips as $trip)
-    @include ('trips.trip')
+    @include ('past_trips.trip')
 @endforeach
 <div style="height:40px"></div>
 @endsection
