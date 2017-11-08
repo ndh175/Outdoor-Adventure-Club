cab4a256232ded2cbce10f833278284f82f1632d
Nick Holbrook <ndh175@gmail.com>
2017-11-08 00:12:27 -0500

    Extra Char

--- resources/views/trips/past_trip.blade.php
+++ resources/views/trips/past_trip.blade.php
@@ -5,7 +5,7 @@
             <div class="col-md-1"></div>
             <div class="col-md-4" style="background-color:rgb(38,38,38);padding:25px;border-radius:10px;">
                 <h2 class="text-center" style="font-family:Allerta, sans-serif;color:rgb(255,255,255);"><del>{{ $trip->name }}</del></h2>
-                <p class="lead text-center" style="margin-bottom:30px;"><<del><strong>Date(s):</strong> @if ($trip->start_date == $trip->end_date) {{ $trip->start_date }} @else {{ $trip->start_date }} - {{ $trip->end_date }} @endif</del></p>
+                <p class="lead text-center" style="margin-bottom:30px;"><del><strong>Date(s):</strong> @if ($trip->start_date == $trip->end_date) {{ $trip->start_date }} @else {{ $trip->start_date }} - {{ $trip->end_date }} @endif</del></p>
                 <p class="lead text-center"><del><strong>Where:</strong> {{ $trip->location }}</del></p>
                 <p class="lead text-center" style="margin-bottom:30px;"><del><strong>Price:</strong> ${{ $trip->price }}</del></p>
                 <p class="lead text-center" style="margin-bottom:30px;"><del><strong>Spots Left: </strong> {{ $trip->remaining_spots }} / {{ $trip->total_spots }}</del></p>
