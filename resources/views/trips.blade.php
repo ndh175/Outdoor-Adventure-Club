bccf598410fc3a3938ccb7391a0107c31b814e11
Nick Holbrook <ndh175@gmail.com>
2017-11-07 23:51:17 -0500

    Test Update

--- resources/views/trips.blade.php
+++ resources/views/trips.blade.php
@@ -1,11 +1,8 @@
 @extends ('layouts.master')
 
 @section('content')
-@foreach ($future_trips as $trip)
+@foreach ($trips as $trip)
     @include ('trips.trip')
-@endforeach
-@foreach ($past_trips as $trip)
-    @include ('past_trips.trip')
 @endforeach
 <div style="height:40px"></div>
 @endsection
