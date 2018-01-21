436fd2d445ab676218b72d58537b57c7c7d7f806
Nick Holbrook <ndh175@gmail.com>
2018-01-21 17:39:23 -0500

    Updated FAQs Page

--- resources/views/faqs.blade.php
+++ resources/views/faqs.blade.php
@@ -1,40 +1,60 @@
 @extends ('layouts.master')
 
 @section ('content')
-<div class="row" style="margin-top:50px;margin-left:0px;">
-    <div class="col-md-3"></div>
-    <div class="col-md-6" style="padding-bottom:25px;">
-        <h1>Faq's </h1>
-        <div style="margin-top:30px;">
-            <h2 style="margin-bottom:20px;"><strong>How do i join?</strong></h2>
-            <p style="font-size:21px;">The best way to become an active member is to pay dues, come to meetings, and go on trips! The best way to hear about upcoming events is to follow our social media accounts and find us on Grizzorgs. </p>
-            <hr>
+<div class="app-body">
+
+  <!-- Main content -->
+  <main class="main">
+
+    <div class="container-fluid">
+
+      <div class="animated fadeIn">
+
+        <div class="card-columns mb-4 mt-4">
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">How do I join?</h4>
+              <p class="card-text">The best way to become an active member is to pay dues, come to meetings, and go on trips! The best way to hear about upcoming events is to follow our social media accounts and find us on Grizzorgs.</p>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">What if I don't have gear?</h4>
+              <p class="card-text">By paying dues you will have access to our gear for free! So even if you have little to no gear, you can still participate in events and trips.</p>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">How much are dues and what do I get out of paying them?</h4>
+              <p class="card-text">Dues are $15 per semester, what you get are:
+                <ul>
+                  <li>Early registration for trips</li>
+                  <li>15% discount at Moosejaw</li>
+                  <li>Free gear rentals from the org</li>
+                  <li>Free shirt</li>
+                </ul>
+              </p>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">What if I don't have any experience?</h4>
+              <p class="card-text">Thats okay! We have all levels of trips ranging from minimal day-trips, to week long backwoods backpacking trips. One of the main goals of our organization is to be able to offer activities for everyone.</p>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">If you have any other questions or concerns please contact Luis Cervantes (President) at luiscervantes@oakland.edu.</h4>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
         </div>
-        <div style="margin-top:30px;">
-            <h2 style="margin-bottom:20px;"><strong>How much are dues and what do I get out of paying them?</strong> </h2>
-            <p style="font-size:21px;">Dues are $15 per semester, what you get are:</p>
-            <ul style="font-size:21px;">
-                <li>Early registration for trips</li>
-                <li>15% discount at Moosejaw</li>
-                <li>Free gear rentals from the org</li>
-                <li>Free shirt</li>
-            </ul>
-            <hr>
-        </div>
-        <div style="margin-top:30px;">
-            <h2 style="margin-bottom:20px;"><strong>What if I don't have gear?</strong> </h2>
-            <p style="font-size:21px;">By paying dues you will have access to our gear for free! So even if you have little to no gear, you can still participate in events and trips. </p>
-            <hr>
-        </div>
-        <div style="margin-top:30px;">
-            <h2 style="margin-bottom:20px;"><strong>What if I don't have any experience?</strong> </h2>
-            <p style="font-size:21px;">Thats okay! We have all levels of trips ranging from minimal day-trips, to week long backwoods backpacking trips. One of the main goals of our organization is to be able to offer activities for everyone. </p>
-            <hr>
-        </div>
-        <div style="margin-top:30px;">
-            <p style="font-size:21px;">If you have any other questions or concerns please contact Luis Cervantes (President) at <a href="mailto:luiscervantes@oakland.edu">luiscervantes@oakland.edu</a>.</p>
-        </div>
+      </div>
     </div>
-    <div class="col-md-3"></div>
+  </main>
 </div>
 @endsection
