158526d911e0cce45f08b477bb6790a011e386ef
Nick Holbrook <ndh175@gmail.com>
2018-01-21 17:43:49 -0500

    Updated Members Page

--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -1,21 +1,29 @@
 @extends ('layouts.master')
 
 @section ('content')
-<div class="team-clean">
-    <div class="container">
-        <div class="intro">
-            <h2 class="text-center" style="font-family:Allerta, sans-serif;color:rgb(38,38,38);margin-bottom:20px;padding-top:20px;">Members </h2>
-        </div>
-        <div class="row people" style="margin-bottom:30px;padding-bottom:20px;">
-        @foreach ($members as $member)
-            <div class="col-md-2 col-sm-6 item" style="padding-top:25px;">
-                <img class="img-circle" src="{{ asset('/storage/' . substr($member->image_url, 7)) }}" style="width:100px;height:100px;object-fit:cover;overflow:hidden;">
-                <h3 class="name">{{ explode(' ', $member->name)[0] }} </h3>
-                <p class="title">{{ $member->title }}  @if ($member->has_paid) <i class="fa fa-check-square-o" style="font-size:1em;color:#00cc00"></i> @endif </p>
-                <p class="description">{{ $member->joined_date }}</p>
+<div class="app-body">
+
+  <!-- Main content -->
+  <main class="main">
+
+    <div class="container-fluid">
+
+      <div class="animated fadeIn">
+
+        <div class="card-deck mb-4 mt-4">
+          @foreach ($members as $member)
+          <div class="card text-white bg-dark">
+            <img class="card-img-top" src="{{ asset('/storage/' . substr($member->image_url, 7)) }}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title m-0">{{ $member->name }} @if ($member->has_paid)<i class="fa fa-check text-primary"></i>@endif</h5>
+              <p class="card-text m-0"><small class="text-muted">{{ $member->title }}</small></p>
+              <p class="card-text"><small class="text-muted">{{ $member->joined_date }}</small></p>
             </div>
-        @endforeach
+          </div>
+          @endforeach
         </div>
+      </div>
     </div>
+  </main>
 </div>
 @endsection
