07f09b98c57a6c8404da791f824c646faf3ea5f6
Nick Holbrook <ndh175@gmail.com>
2018-01-21 17:15:46 -0500

    Removed Section

--- resources/views/trips.blade.php
+++ resources/views/trips.blade.php
@@ -54,14 +54,3 @@
     </div>
   </main>
 </div>
-
-<!--
-@foreach ($future_trips as $trip)
-    @include ('trips.trip')
-@endforeach
-@foreach ($past_trips as $trip)
-    @include ('trips.past_trip')
-@endforeach
-<div style="height:40px"></div>
-@endsection
--->
