c9e53e3523680b08e388f3b76713ece0ea293245
Nick Holbrook <ndh175@gmail.com>
2018-01-21 17:47:21 -0500

    Image Style Update

--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -12,8 +12,8 @@
 
         <div class="card-columns mb-4 mt-4">
           @foreach ($members as $member)
-          <div class="card text-white bg-dark" style="width:20vh">
-            <img class="card-img-top" src="{{ asset('/storage/' . substr($member->image_url, 7)) }}" alt="Card image cap">
+          <div class="card text-white bg-dark">
+            <img class="card-img-top" src="{{ asset('/storage/' . substr($member->image_url, 7)) }}" alt="Card image cap" style="width:20vh">
             <div class="card-body">
               <h5 class="card-title m-0">{{ $member->name }} @if ($member->has_paid)<i class="fa fa-check text-primary"></i>@endif</h5>
               <p class="card-text m-0"><small class="text-muted">{{ $member->title }}</small></p>
