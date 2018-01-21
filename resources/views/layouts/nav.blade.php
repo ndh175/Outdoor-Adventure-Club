6c1e7b0944574408a48e07f8f47d52e609e18e34
Nick Holbrook <ndh175@gmail.com>
2018-01-21 17:11:58 -0500

    Testing Nav Active Update

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -7,7 +7,7 @@
 
   <ul class="nav navbar-nav d-md-down-none">
     <li class="nav-item px-3">
-      <a class="nav-link active" href="#">Home</a>
+      <a class="nav-link @if ($page == 'www.outdooradventureclubatou.com') active @endif" href="#">Home</a>
     </li>
     <li class="nav-item px-3">
       <a class="nav-link" href="trips.html">Trips</a>
