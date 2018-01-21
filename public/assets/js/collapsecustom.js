0979a14f48588d6e1d8bd91855d90d973aed1d4b
Nick Holbrook <ndh175@gmail.com>
2018-01-21 17:03:32 -0500

    Moved CSS files back

--- public/assets/js/collapsecustom.js
+++
@@ -1,9 +0,0 @@
-function collapsecustom() {
-    jQuery('.collapse').addClass('no-transition').collapse('hide');
-}
-
-$(function() {
-    $('.toggle').click(function(e) {
-        jQuery('.collapse').addClass('no-transition').collapse('hide');
-    });
-  });
