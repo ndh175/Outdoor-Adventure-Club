b27930e7df19b16502adaddc1e674a34603b759b
Nick Holbrook <ndh175@gmail.com>
2018-01-21 18:48:58 -0500

    Updated Eboard Positions

--- resources/views/layouts/eboard_info.blade.php
+++ resources/views/layouts/eboard_info.blade.php
@@ -1,114 +1,169 @@
-<div class="row people collapse" id="positions" style="background-color:rgb(38,38,38);padding-bottom:0px;border-radius:10px;margin-bottom:40px">
-    <div class="col-md-12"></div>
-    <div class="col-md-1" style="padding-right:0px;padding-left:0px;"></div>
-    <div class="col-md-10" style="padding-right:0px;padding-left:0px;">
-        <div>
-            <h3 class="text-nowrap name" style="color:rgb(249,250,251);margin-top:40px;margin-bottom:5px;">President </h3>
-            <ul style="color:rgb(125,130,133);padding-right:10px;">
-                <li>Attending every meeting/ speak at every meeting </li>
-                <li>To coordinate with speakers/ locations for presentations and events </li>
-                <li>Direct the Vice President with the planning of weekly meetings </li>
-                <li>To manage and oversee all other E-board members/ assist with their activity planning </li>
-                <li>To plan at least one Top Level outing/ activity/ trip per year for the club </li>
-                <li>And most importantly, the president should give something unique back to the club to ensure its growth and to set a platform for future presidents and members </li>
-                <li>Must attend CSA Training </li>
-            </ul>
+<div class="app-body">
+
+  <!-- Main content -->
+  <main class="main">
+
+    <div class="container-fluid">
+
+      <div class="animated fadeIn">
+
+        <div class="card-columns mt-4 mb-4">
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">President</h4>
+              <p class="card-text">
+                <ul>
+                  <li>Attending every meeting / speak at every meeting.</li>
+                  <li>To coordinate with speakers / locations for presentations and events.</li>
+                  <li>Direct the Vice President with the planning of weekly meetings.</li>
+                  <li>To manage and oversee all other E-board members / assist with their activity planning.</li>
+                  <li>To plan at least one Top Level outing / activity / trip per year for the club.</li>
+                  <li>And most importantly, the president should give something unique back to the club to ensure its growth and to set a platform for future presidents and members.</li>
+                  <li>Must attend CSA Training.</li>
+                </ul>
+              </p>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">Vice President</h4>
+              <p class="card-text">
+                <ul>
+                  <li>The Vice President's job is to assist the president by helping out with club events and holding E-board members accountable for their duties.</li>
+                  <li>Working with the President and Secretary to plan weekly meetings.</li>
+                  <li>Direct oversight / point of contact for club members as well as E-Board.</li>
+                  <li>Must plan at least one outing / activity / trip per year (Fall Trip).</li>
+                  <li>Must attend CSA Training.</li>
+                </ul>
+              </p>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">Treasurer</h4>
+              <p class="card-text">
+                <ul>
+                  <li>Maintain and keep track of membership cards and dues.</li>
+                  <li>Collecting money from members.</li>
+                  <li>Cash checks as well as maintain accounts with CSA.</li>
+                  <li>Request funding and project / estimate spending for trips as well as events.</li>
+                  <li>Making purchases for club through CSA.</li>
+                  <li>Must plan at least one outing / activity / trip per semester.</li>
+                  <li>Must attend CSA Training.</li>
+                </ul>
+              </p>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">Secretary</h4>
+              <p class="card-text">
+                <ul>
+                  <li>Take notes at every meeting.</li>
+                  <li>Fill out and turn in Equipment request forms through CSA.</li>
+                  <li>E-mail club members updates / itineraries for trips, meetings, etc.</li>
+                  <li>Assist other E-board members with tasks including:</li>
+                  <ul>
+                    <li>Helping with the making of trip packets for club members.</li>
+                    <li>Research on locations for trips.</li>
+                    <li>Reserving sites for trips.</li>
+                    <li>Keeping up to date on the policies and procedures that are to be followed at each site, or location the club travels to.</li>
+                  </ul>
+                  <li>Update email list on Google Drive.</li>
+                  <li>Assist the Vice President in the creation of itineraries for weekly meetings.</li>
+                  <li>Help the Webmaster with posting a schedule of events and a summary of previous meetings on the website.</li>
+                  <li>Must maintain Google Calendar by posting events and sending invites.</li>
+                  <li>Must plan at least one outing / activity / trip per semester.</li>
+                  <li>Must attend CSA Training.</li>
+                </ul>
+              </p>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">Public Relations</h4>
+              <p class="card-text">
+                <ul>
+                  <li>Coordinate tabling at events to increase membership (Go for the Gold, Student Organization Fairs, etc).</li>
+                  <li>Hang approved flyers created by the Creative Directors and Fundraising.</li>
+                  <li>Maintain social media accounts by posting regularly and increasing followers by using the photos uploaded to Google Drive by the Creative Directors.</li>
+                  <li>Pass on passwords / account info to next PR & SO member.</li>
+                  <li>Must plan at least one outing / activity / trip per semester.</li>
+                </ul>
+              </p>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">Safety Director</h4>
+              <p class="card-text">
+                <ul>
+                  <li>Must check trips for safety issues.</li>
+                  <li>Handles Emergency Fund.</li>
+                  <li>Makes sure liability waivers are signed and up to date.</li>
+                  <li>Must plan at least one outing / activity / trip per semester.</li>
+                </ul>
+              </p>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">Creative Director</h4>
+              <p class="card-text">
+                <ul>
+                  <li>Take photos / videos at major events & upload to Google Drive.</li>
+                  <li>Work with the Fundraising and PR & SO to develop / create flyers for future events.</li>
+                  <li>Coordinate with club members to collect photos / media.</li>
+                  <li>Coordinate with PR & SO to upload photos to social media.</li>
+                  <li>Coordinate with the Webmaster to upload photos / videos to the website.</li>
+                  <li>Assist in the creation of any designs needed for club (membership cards, t-shirts, flyers, poster boards, etc).</li>
+                  <li>Maintain our club website and GrizzOrgs.</li>
+                  <li>Work alongside the Secretary to post upcoming events/a calendar and previous meeting summaries on the website and GrizzOrgs.</li>
+                  <li>Must plan at least one outing / activity / trip per semester.</li>
+                </ul>
+              </p>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">Fundraising</h4>
+              <p class="card-text">
+                <ul>
+                  <li>Must contact nearby stores and businesses in search of sponsors / donations.</li>
+                  <li>Must organize fundraising tables / events.</li>
+                  <li>Coordinate with Treasurer / other E-Board members to set up fundraising events / activities.</li>
+                  <li>Must maintain relationship with business contacts as well as student body (flyers, etc.) Coordinate with Creative Directors to plan and publicize events.</li>
+                  <li>Must plan at least one activity / outing / trip per semester.</li>
+                </ul>
+              </p>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body d-flex flex-column">
+              <h4 class="card-title">Gear Manager</h4>
+              <p class="card-text">
+                <ul>
+                  <li>Must maintain inventory (Inventory History, etc).</li>
+                  <li>Coordinate with Treasurer and other E-Board members to keep list of paying members updated.</li>
+                  <li>Create and update a google form for members to sign out/ reserve gear whenever they would like.</li>
+                  <li>Store & retrieve gear for members.</li>
+                  <li>Check out gear to members.</li>
+                  <li>Must plan at least one outing/ activity/ trip per semester.</li>
+                </ul>
+              </p>
+              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+            </div>
+          </div>
         </div>
-        <div>
-            <h3 class="text-nowrap name" style="color:rgb(255,255,255);margin-top:20px;margin-bottom:5px;">Vice President</h3>
-            <ul style="color:rgb(125,130,133);padding-right:10px;">
-                <li>The Vice President's job is to assist the president by helping out with club events and holding E-board members accountable for their duties. </li>
-                <li>Working with the President and Secretary to plan weekly meetings </li>
-                <li>Direct oversight/ point of contact for club members as well as E-Board </li>
-                <li>Must plan at least one outing/ activity/ trip per year (Fall Trip) </li>
-                <li>Must attend CSA Training </li>
-            </ul>
-        </div>
-        <div>
-            <h3 class="text-nowrap name" style="color:rgb(250,250,251);margin-top:20px;margin-bottom:5px;">Treasurer </h3>
-            <ul style="color:rgb(125,130,133);padding-right:10px;">
-                <li>Maintain and keep track of membership cards and dues </li>
-                <li>Collecting money from members </li>
-                <li>Cash checks as well as maintain accounts with CSA </li>
-                <li>Request funding and project/ estimate spending for trips as well as events </li>
-                <li>Making purchases for club through CSA </li>
-                <li>Must plan at least one outing/ activity/ trip per semester </li>
-                <li>Must attend CSA Training </li>
-            </ul>
-        </div>
-        <div>
-            <h3 class="text-nowrap name" style="color:rgb(252,252,252);margin-top:20px;margin-bottom:5px;">Secretary </h3>
-            <ul style="color:rgb(125,130,133);padding-right:10px;">
-                <li>Take notes at every meeting </li>
-                <li>Fill out and turn in Equipment request forms through CSA </li>
-                <li>E-mail club members updates/ itineraries for trips, meetings, etc. </li>
-                <li>Assist other E-board members with tasks including:</li>
-                <li><em>Helping with the making of trip packets for club members</em> </li>
-                <li><em>Research on locations for trips</em> </li>
-                <li><em>Reserving sites for trips</em> </li>
-                <li><em>Keeping up to date on the policies and procedures that are to be followed at each site, or location the club travels to</em> </li>
-                <li>Update email list on Google Drive </li>
-                <li>Assist the Vice President in the creation of itineraries for weekly meetings </li>
-                <li>Help the Webmaster with posting a schedule of events and a summary of previous meetings on the website </li>
-                <li>Must maintain Google Calendar by posting events and sending invites </li>
-                <li> Must plan at least one outing/ activity/ trip per semester</li>
-                <li>Must attend CSA Training </li>
-            </ul>
-        </div>
-        <div>
-            <h3 class="text-nowrap name" style="color:rgb(252,252,252);margin-top:20px;margin-bottom:5px;">Public Relations </h3>
-            <ul style="color:rgb(125,130,133);padding-right:10px;">
-                <li>Coordinate tabling at events to increase membership (Go for the Gold, Student Organization Fairs, etc) </li>
-                <li>Hang approved flyers created by the Creative Directors and Fundraising </li>
-                <li>Maintain social media accounts by posting regularly and increasing followers by using the photos uploaded to Google Drive by the Creative Directors </li>
-                <li>Pass on passwords/ account info to next PR &amp; SO member </li>
-                <li>Must plan at least one outing/ activity/ trip per semester </li>
-            </ul>
-        </div>
-        <div>
-            <h3 class="text-nowrap name" style="color:rgb(252,252,252);margin-top:20px;margin-bottom:5px;">Creative Director </h3>
-            <ul style="color:rgb(125,130,133);padding-right:10px;">
-                <li>Take photos/ videos at major events &amp; upload to Google Drive </li>
-                <li>Work with the Fundraising and PR &amp; SO to develop/create flyers for future events </li>
-                <li>Coordinate with club members to collect photos/media </li>
-                <li>Coordinate with PR &amp; SO to upload photos to social media </li>
-                <li>Coordinate with the Webmaster to upload photos/videos to the website </li>
-                <li>Assist in the creation of any designs needed for club (membership cards, t-shirts, flyers, poster boards, etc.) </li>
-                <li>Maintain our club website and GrizzOrgs </li>
-                <li>Work alongside the Secretary to post upcoming events/a calendar and previous meeting summaries on the website and GrizzOrgs </li>
-                <li>Must plan at least one outing/ activity/ trip per semester </li>
-            </ul>
-        </div>
-        <div>
-            <h3 class="text-nowrap name" style="color:rgb(252,252,252);margin-top:20px;margin-bottom:5px;">Fundraising </h3>
-            <ul style="color:rgb(125,130,133);padding-right:10px;">
-                <li>Must contact nearby stores and businesses in search of sponsors/ donations </li>
-                <li>Must organize fundraising tables/ events </li>
-                <li>Coordinate with Treasurer/ other E-Board members to set up fundraising events/ activities </li>
-                <li>Must maintain relationship with business contacts as well as student body (flyers, etc.) Coordinate with Creative Directors to plan and publicize events </li>
-                <li>Must plan at least one activity/ outing/ trip per semester </li>
-            </ul>
-        </div>
-        <div>
-            <h3 class="text-nowrap name" style="color:rgb(252,252,252);margin-top:20px;margin-bottom:5px;">Gear Manager </h3>
-            <ul style="color:rgb(125,130,133);padding-right:10px;">
-                <li>Must maintain inventory (Inventory History, etc) </li>
-                <li>Coordinate with Treasurer and other E-Board members to keep list of paying members updated </li>
-                <li>Create and update a google form for members to sign out/ reserve gear whenever they would like </li>
-                <li>Store &amp; retrieve gear for members </li>
-                <li>Check out gear to members </li>
-                <li>Must plan at least one outing/ activity/ trip per semester </li>
-            </ul>
-        </div>
-        <div>
-            <h3 class="text-nowrap name" style="color:rgb(252,252,252);margin-top:20px;margin-bottom:5px;">Safety Director </h3>
-            <ul style="color:rgb(125,130,133);padding-right:10px;padding-bottom:40px;">
-                <li>Must check trips for safety issues </li>
-                <li>Handles Emergency Fund </li>
-                <li>Makes sure liability waivers are signed and up to date. </li>
-                <li>Must plan at least one outing/ activity/ trip per semester </li>
-            </ul>
-        </div>
+      </div>
     </div>
-    <div class="col-md-1" style="padding-right:0px;padding-left:0px;"></div>
+  </main>
 </div>
