e304fb2c0069fe0fbd800ef13520cf8b990a59aa
Nick Holbrook <ndh175@gmail.com>
2018-01-21 18:14:08 -0500

    Updated Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,26 +1,44 @@
 @extends ('layouts.master')
 
 @section ('content')
-    <div></div>
-    <div class="login-clean" style="background-color:rgb(255,255,255);">
-        <form action="/login" method="POST" style="background-color:rgb(38,38,38);border-radius:10px">
-            {{ csrf_field() }}
-            <h2 class="sr-only">Login Form</h2>
-            <div class="illustration" style="color:rgb(82,94,100);"><img class="img-rounded" src="assets/img/logo4.jpg" height="150"></div>
-            <div class="form-group">
-                <input class="form-control" type="text" name="email" placeholder="Email" style="background-color:rgb(38,38,38);color:#FFF">
+  <body class="app flex-row align-items-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card-group">
+            <div class="card p-4">
+              <div class="card-body">
+                <h1>Login</h1>
+                <p class="text-muted">Sign In to your account</p>
+                <div class="input-group mb-3">
+                  <span class="input-group-addon"><i class="icon-user"></i></span>
+                  <input type="text" class="form-control" placeholder="Username">
+                </div>
+                <div class="input-group mb-4">
+                  <span class="input-group-addon"><i class="icon-lock"></i></span>
+                  <input type="password" class="form-control" placeholder="Password">
+                </div>
+                <div class="row">
+                  <div class="col-6">
+                    <button type="button" class="btn btn-primary px-4">Login</button>
+                  </div>
+                  <div class="col-6 text-right">
+                    <button type="button" class="btn btn-link px-0">Forgot password?</button>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="form-group">
-                <input class="form-control" type="password" name="password" placeholder="Password" style="background-color:rgb(38,38,38);color:#FFF">
+            <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
+              <div class="card-body text-center">
+                <div>
+                  <h2>Sign up</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <button type="button" class="btn btn-primary active mt-3">Register Now!</button>
+                </div>
+              </div>
             </div>
-            <!--<a href="#" class="forgot">Forgot your email or password?</a>-->
-            <div class="form-group">
-                <button class="btn btn-success btn-block outline" type="submit" style="margin-top:15px;">Log In</button>
-            </div>
-            <div class="form-group">
-                <a href="register" class="forgot">Don't have an account? Register here</a>
-            </div>
-            @include ('layouts.errors')
-            </form>
+          </div>
+        </div>
+      </div>
     </div>
 @endsection
