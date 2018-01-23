c74a802b3814a5e806a071f4ac0149d7f77a1038
Nick Holbrook <ndh175@gmail.com>
2018-01-22 19:44:59 -0500

    Fixed Sidebar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -44,11 +44,11 @@
           <nav class="sidebar-nav">
             <ul class="nav">
               <li class="nav-item">
-                <a class="nav-link" href="main.html"><i class="icon-speedometer"></i> Dashboard <span class="badge badge-primary">NEW</span></a>
+                <a class="nav-link" href="main.html"><i class="icon-speedometer"></i> Home <span class="badge badge-primary">NEW</span></a>
               </li>
 
               <li class="nav-title">
-                UI Elements
+                Other
               </li>
               <li class="nav-item nav-dropdown">
                 <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-puzzle"></i> Components</a>
@@ -79,51 +79,6 @@
                   </li>
                 </ul>
               </li>
-              <li class="nav-item nav-dropdown">
-                <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-star"></i> Icons</a>
-                <ul class="nav-dropdown-items">
-                  <li class="nav-item">
-                    <a class="nav-link" href="icons/font-awesome.html"><i class="icon-star"></i> Font Awesome <span class="badge badge-secondary">4.7</span></a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="icons/simple-line-icons.html"><i class="icon-star"></i> Simple Line Icons</a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="widgets.html"><i class="icon-calculator"></i> Widgets <span class="badge badge-primary">NEW</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="charts.html"><i class="icon-pie-chart"></i> Charts</a>
-              </li>
-              <li class="divider"></li>
-              <li class="nav-title">
-                Extras
-              </li>
-              <li class="nav-item nav-dropdown">
-                <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-star"></i> Pages</a>
-                <ul class="nav-dropdown-items">
-                  <li class="nav-item">
-                    <a class="nav-link" href="views/pages/login.html" target="_top"><i class="icon-star"></i> Login</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="views/pages/register.html" target="_top"><i class="icon-star"></i> Register</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="views/pages/404.html" target="_top"><i class="icon-star"></i> Error 404</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="views/pages/500.html" target="_top"><i class="icon-star"></i> Error 500</a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item mt-auto">
-                <a class="nav-link nav-link-success" href="http://coreui.io" target="_top"><i class="icon-cloud-download"></i> Download CoreUI</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link nav-link-danger" href="http://coreui.io/pro/" target="_top"><i class="icon-layers"></i> Try CoreUI <strong>PRO</strong></a>
-              </li>
-
             </ul>
           </nav>
           <button class="sidebar-minimizer brand-minimizer" type="button"></button>
