8da195626edd9abccf5e460c45b8e8af880928d1
Nick Holbrook <ndh175@gmail.com>
2018-01-22 20:08:12 -0500

    Updated Gallery

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,57 +1,51 @@
 @extends ('layouts.master')
 
 @section('content')
-<div class="app-body">
-<!-- Main content -->
-<main class="main">
+<div class="container-fluid">
 
-  <div class="container-fluid">
+  <div class="animated fadeIn">
 
-    <div class="animated fadeIn">
-
-      <div class="card-columns mb-4 mt-4">
-        <div class="card text-white bg-dark">
-            <img class="card-img-top" src="img/canyon.jpg" alt="Card image">
-            <div class="card-body">
-              <h4 class="card-title">Grand Canyon</h4>
-              <p class="card-text">4/05/2017</p>
-              <p class="card-text"><small class="text-muted">27 Pictures</small></p>
-            </div>
-        </div>
-        <div class="card text-white bg-dark">
-            <img class="card-img-top" src="img/cabin.jpeg" alt="Card image">
-            <div class="card-body">
-              <h4 class="card-title">Cabin Trip</h4>
-              <p class="card-text">4/05/2017</p>
-              <p class="card-text"><small class="text-muted">27 Pictures</small></p>
-            </div>
-        </div>
-        <div class="card text-white bg-dark">
-            <img class="card-img-top" src="img/trips/beach.jpg" alt="Card image">
-            <div class="card-body">
-              <h4 class="card-title">Beach</h4>
-              <p class="card-text">4/05/2017</p>
-              <p class="card-text"><small class="text-muted">27 Pictures</small></p>
-            </div>
-        </div>
-        <div class="card text-white bg-dark">
-            <img class="card-img-top" src="img/dunes.jpg" alt="Card image">
-            <div class="card-body">
-              <h4 class="card-title">Nordhouse Dunes</h4>
-              <p class="card-text">4/05/2017</p>
-              <p class="card-text"><small class="text-muted">27 Pictures</small></p>
-            </div>
-        </div>
-        <div class="card text-white bg-dark">
-            <img class="card-img-top" src="img/trips/lake.jpg" alt="Card image">
-            <div class="card-body">
-              <h4 class="card-title">Lake</h4>
-              <p class="card-text">4/05/2017</p>
-              <p class="card-text"><small class="text-muted">27 Pictures</small></p>
-            </div>
-        </div>
+    <div class="card-columns mb-4 mt-4">
+      <div class="card text-white bg-dark">
+          <img class="card-img-top" src="img/canyon.jpg" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">Grand Canyon</h4>
+            <p class="card-text">4/05/2017</p>
+            <p class="card-text"><small class="text-muted">27 Pictures</small></p>
+          </div>
+      </div>
+      <div class="card text-white bg-dark">
+          <img class="card-img-top" src="img/cabin.jpeg" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">Cabin Trip</h4>
+            <p class="card-text">4/05/2017</p>
+            <p class="card-text"><small class="text-muted">27 Pictures</small></p>
+          </div>
+      </div>
+      <div class="card text-white bg-dark">
+          <img class="card-img-top" src="img/trips/beach.jpg" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">Beach</h4>
+            <p class="card-text">4/05/2017</p>
+            <p class="card-text"><small class="text-muted">27 Pictures</small></p>
+          </div>
+      </div>
+      <div class="card text-white bg-dark">
+          <img class="card-img-top" src="img/dunes.jpg" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">Nordhouse Dunes</h4>
+            <p class="card-text">4/05/2017</p>
+            <p class="card-text"><small class="text-muted">27 Pictures</small></p>
+          </div>
+      </div>
+      <div class="card text-white bg-dark">
+          <img class="card-img-top" src="img/trips/lake.jpg" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">Lake</h4>
+            <p class="card-text">4/05/2017</p>
+            <p class="card-text"><small class="text-muted">27 Pictures</small></p>
+          </div>
       </div>
     </div>
-  </main>
-</div>
+  </div>
 @endsection
