ac07911596f2a890ab2e45358424d00a68c27cb8
Nick Holbrook <ndh175@gmail.com>
2018-01-22 19:34:06 -0500

    Fixed Right View

--- resources/views/eboard.blade.php
+++ resources/views/eboard.blade.php
@@ -99,28 +99,6 @@
               <p class="card-text mt-2">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa.</p>
             </div>
           </div>
-          <div class="card text-white bg-dark">
-            <div class="card-header">
-              <h4 class="card-title m-0">Safety Director</h4>
-            </div>
-            <img class="card-img-top" src="img/avatars/micheal.jpg" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title m-0">Micheal Cloutier <i class="fa fa-check text-primary"></i></h5>
-              <p class="card-text m-0"><small class="text-muted">Joined November 06, 2017</small></p>
-              <p class="card-text mt-2">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa.</p>
-            </div>
-          </div>
-          <div class="card text-white bg-dark">
-            <div class="card-header">
-              <h4 class="card-title m-0">Web Master</h4>
-            </div>
-            <img class="card-img-top" src="img/avatars/micheal.jpg" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title m-0">N/A <i class="fa fa-check text-primary"></i></h5>
-              <p class="card-text m-0"><small class="text-muted">Joined November 06, 2017</small></p>
-              <p class="card-text mt-2">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa.</p>
-            </div>
-          </div>
         </div>
       </div>
     </main>
