29f43872ff1898d71ca94d426db45877d8d9ef15
Nick Holbrook <ndh175@gmail.com>
2018-01-23 00:13:47 -0500

    Updated Trips

--- resources/views/trips.blade.php
+++ resources/views/trips.blade.php
@@ -3,7 +3,10 @@
 @section('content')
 <div class="container-fluid">
   <div class="animated fadeIn">
-    @foreach ($trips as $trip)
+    @foreach ($future_trips as $trip)
+        @include ('trips.trip')
+    @endforeach
+    @foreach ($past_trips as $trip)
         @include ('trips.trip')
     @endforeach
   </div>
