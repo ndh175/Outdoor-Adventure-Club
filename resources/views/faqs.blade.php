2b5a255676966d59ffd6d617e2974f774af1479b
Nick Holbrook <ndh175@gmail.com>
2018-01-22 20:09:48 -0500

    Updated FAQs

--- resources/views/faqs.blade.php
+++ resources/views/faqs.blade.php
@@ -1,60 +1,53 @@
 @extends ('layouts.master')
 
 @section ('content')
-<div class="app-body">
+<div class="container-fluid">
 
-  <!-- Main content -->
-  <main class="main">
+  <div class="animated fadeIn">
 
-    <div class="container-fluid">
-
-      <div class="animated fadeIn">
-
-        <div class="card-columns mb-4 mt-4">
-          <div class="card">
-            <div class="card-body d-flex flex-column">
-              <h4 class="card-title">How do I join?</h4>
-              <p class="card-text">The best way to become an active member is to pay dues, come to meetings, and go on trips! The best way to hear about upcoming events is to follow our social media accounts and find us on Grizzorgs.</p>
-              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body d-flex flex-column">
-              <h4 class="card-title">What if I don't have gear?</h4>
-              <p class="card-text">By paying dues you will have access to our gear for free! So even if you have little to no gear, you can still participate in events and trips.</p>
-              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body d-flex flex-column">
-              <h4 class="card-title">How much are dues and what do I get out of paying them?</h4>
-              <p class="card-text">Dues are $15 per semester, what you get are:
-                <ul>
-                  <li>Early registration for trips</li>
-                  <li>15% discount at Moosejaw</li>
-                  <li>Free gear rentals from the org</li>
-                  <li>Free shirt</li>
-                </ul>
-              </p>
-              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body d-flex flex-column">
-              <h4 class="card-title">What if I don't have any experience?</h4>
-              <p class="card-text">Thats okay! We have all levels of trips ranging from minimal day-trips, to week long backwoods backpacking trips. One of the main goals of our organization is to be able to offer activities for everyone.</p>
-              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body d-flex flex-column">
-              <h4 class="card-title">If you have any other questions or concerns please contact Luis Cervantes (President) at luiscervantes@oakland.edu.</h4>
-              <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
-            </div>
-          </div>
+    <div class="card-columns mb-4 mt-4">
+      <div class="card">
+        <div class="card-body d-flex flex-column">
+          <h4 class="card-title">How do I join?</h4>
+          <p class="card-text">The best way to become an active member is to pay dues, come to meetings, and go on trips! The best way to hear about upcoming events is to follow our social media accounts and find us on Grizzorgs.</p>
+          <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body d-flex flex-column">
+          <h4 class="card-title">What if I don't have gear?</h4>
+          <p class="card-text">By paying dues you will have access to our gear for free! So even if you have little to no gear, you can still participate in events and trips.</p>
+          <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body d-flex flex-column">
+          <h4 class="card-title">How much are dues and what do I get out of paying them?</h4>
+          <p class="card-text">Dues are $15 per semester, what you get are:
+            <ul>
+              <li>Early registration for trips</li>
+              <li>15% discount at Moosejaw</li>
+              <li>Free gear rentals from the org</li>
+              <li>Free shirt</li>
+            </ul>
+          </p>
+          <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body d-flex flex-column">
+          <h4 class="card-title">What if I don't have any experience?</h4>
+          <p class="card-text">Thats okay! We have all levels of trips ranging from minimal day-trips, to week long backwoods backpacking trips. One of the main goals of our organization is to be able to offer activities for everyone.</p>
+          <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body d-flex flex-column">
+          <h4 class="card-title">If you have any other questions or concerns please contact Luis Cervantes (President) at luiscervantes@oakland.edu.</h4>
+          <p class="card-text mt-auto"><small class="text-muted">Last updated Jan 11, 2018</small></p>
         </div>
       </div>
     </div>
-  </main>
+  </div>
 </div>
 @endsection
